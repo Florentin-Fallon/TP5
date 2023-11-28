84dd1f3b1cebdb8f70fed95858f2694418d3913e
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2023-11-28 14:48:24 +0100

    tp5

--- tp5_enc_server_1.py
+++ tp5_enc_server_1.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('127.0.0.1', 9999))  
+s.bind(('192.168.64.37', 9999))  
 
 s.listen(1)
 conn, addr = s.accept()
