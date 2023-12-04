87db1ab70cf26cf1fef974d3a075e7acf2aa12ff
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2023-12-04 08:58:13 +0100

    finsih tp

--- tp5_web_client_2.py
+++ tp5_web_client_2.py
@@ -3,7 +3,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
-port = 1111
+port = 3040
 s.connect((host, port))
 
 s.send(b"GET / \r\nHost: localhost\r\n\r\n")
--- tp5_web_serv_3.py
+++ tp5_web_serv_3.py
@@ -1,25 +1,26 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 3033))
+s.bind(('', 3040))
 
 s.listen(1)
-conn, addr = s.accept()
-print(f"Connexion établie avec {addr}")
+
 
 while True:
         try:
+            conn, addr = s.accept()
+            print(f"Connexion établie avec {addr}")
             data = conn.recv(1024)
             print(f"Données reçues du client : {data}")
             if not data: 
                  break
-            file = open("index.html", "rb")
+            file = open("index.html")
             html = file.read()
+            file.close()
             http_header = "HTTP/1.1 200 OK\n\nContent-Type: text/html\r\n\r\n" + html
             s.send(http_header.encode())
-            file.close()
+            conn.close()
         except socket.error as e:
             print(e)
             break
 
-conn.close()
